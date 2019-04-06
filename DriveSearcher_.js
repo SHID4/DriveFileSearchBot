a7246b27c41147b5d4e9b2df5c38ea3db465408a
ishidashota <c611621050@tokushima-u.ac.jp>
2019-04-07 00:05:48 +0900

    検索ワードに一致するファイルを検索できるようにした,README更新

--- DriveSearcher_.js
+++ DriveSearcher_.js
@@ -1,14 +1,18 @@
+// slack API Token
 var slack_token =
-  "xoxp-000000000000000000000000000";
-  
-var validToken = "hogehogehogehoge";
-function saveToken() {
+  "________________";
+// Outgoing Webhook Verify Token
+var validToken = "____________";
+var bikkuri = ":exclamation::exclamation:";
+
+function saveSlackToken() {
   PropertiesService.getScriptProperties().setProperty(
     "slackToken",
     slack_token
   );
 }
 
+// Save Outgoing Webhook Verify Token
 function saveVerifyToken() {
   PropertiesService.getScriptProperties().setProperty(
     "verifyToken",
@@ -16,7 +20,6 @@ function saveVerifyToken() {
   );
 }
 
-// POSTデータを受け取る
 function doGet(e) {}
 
 function doPost(e) {
@@ -26,11 +29,11 @@ function doPost(e) {
     e = {
       parameter: {
         token: prop.verifyToken,
-        team_id: "t00001",
+        team_id: "tuvalun-script",
         channel_id: "CHQFWKE6R",
-        channel_name: "#channel",
+        channel_name: "#bot",
         timestamp: "1355517523.000005",
-        user_id: "id1213131",
+        user_id: "BHFB8N23T",
         user_name: "Steve",
         text: "MyFirstBot: Hi",
         trigger_word: "b"
@@ -45,21 +48,54 @@ function doPost(e) {
   //Create an instance.
   var slackApp = SlackApp.create(prop.slackToken);
 
-  var fileName = e.parameter.text.trim().slice(1);
-  var fileUrl = SearchFile(fileName);
-  SendSearchedMessage(slackApp, "ファイルサーチくん", e, fileUrl);
+  slackApp.chatPostMessage(
+    e.parameter.channel_id,
+    "今からファイルを検索するよ" + bikkuri,
+    {
+      username: "ファイルサーチくん",
+      icon_emoji: ":eyes:"
+    }
+  );
+
+  var triggerWordLength = 1;
+  var fileName = e.parameter.text.trim().slice(triggerWordLength);
+  var fileUrls = SearchFiles(fileName);
+  SendSearchedMessage(slackApp, "ファイルサーチくん", e, fileUrls);
 
   return null;
 }
 
-function SendSearchedMessage(slackApp, userName, e, fileUrl) {
+function SendSearchedMessage(slackApp, userName, e, fileUrls) {
   var baseMsg =
-    "お待たせしました！！あなたが探しているファイルはズバリこれですね！！";
-  var url = "URL:" + fileUrl;
+    "@" +
+    e.parameter.user_name +
+    "\n" +
+    "お待たせ" +
+    bikkuri +
+    "\nあなたが探してそうなファイルを持ってきたよ:eyes::eyes:\n" +
+    "*Hit: " +
+    fileUrls.length / 3 +
+    "件*\n";
+  if (fileUrls.length == 0) {
+    baseMsg = "@" + e.parameter.user_name + "\n" + "そのファイルなかった...";
+  }
+
+  // 3要素で1単位の情報なのでlengthは1/3
+  var url = "";
+
+  for (var i = 0; i < fileUrls.length; i += 3) {
+    var fileUrl = fileUrls[i];
+    var name = fileUrls[i + 1];
+    var folderName = fileUrls[i + 2];
+
+    url += "ファイルの場所: " + folderName + "/*" + name + "*\n";
+    url += "URL: " + fileUrl + "\n";
+  }
 
   slackApp.chatPostMessage(e.parameter.channel_id, baseMsg + "\n" + url, {
     username: userName,
-    icon_emoji: ":eyes:"
+    icon_emoji: ":eyes:",
+    link_names: 1
   });
 }
 
@@ -71,4 +107,60 @@ function SearchFile(fileName) {
   }
 
   return fileInfo.getUrl();
+}
+
+// 外に出すことで再帰でGetPathNameを使うときに状態を保ったまま再帰させる
+var folderNames = [];
+
+// 複数のファイルを検索する(任意の文字が含まれているファイル情報を返す）
+// fileUrls[0 + n * 3] : 存在するファイルのURL
+// fileUrls[1 + n * 3] : 条件に合致するファイル名
+// fileUrls[2 + n * 3] : そのファイルまでのパス
+function SearchFiles(fileName) {
+  var fileUrls = [];
+
+  try {
+    var allFiles = DriveApp.getFiles();
+    while (allFiles.hasNext()) {
+      var file = allFiles.next();
+
+      if (file.getName().indexOf(fileName) != -1) {
+        var path = GetPathName(file.getParents());
+
+        fileUrls.push(file.getUrl());
+        fileUrls.push(file.getName());
+        fileUrls.push(path);
+      }
+      // 次のループのためにクリアする(書かないとGetPathNameが失敗する)
+      folderNames = [];
+    }
+  } catch (e) {
+    return "error:" + e;
+  }
+  return fileUrls;
+}
+
+function GetPathName(folders) {
+  if (folders.hasNext()) {
+    var folder = folders.next();
+    folderNames.push("/" + folder.getName());
+    // さらに一つ上の階層を検索する
+    GetPathName(folder.getParents());
+  }
+
+  var path = JoinReverseArrayString(folderNames);
+  try {
+    return path.slice(1);
+  } catch (e) {
+    return "";
+  }
+}
+
+// 文字列配列を逆から結合する
+function JoinReverseArrayString(strs) {
+  var reverse = "";
+  for (var i = strs.length - 1; i >= 0; i--) {
+    reverse += strs[i];
+  }
+  return reverse;
 }
--- README.md
+++ README.md
@@ -2,5 +2,26 @@
 ## 概要
 slackからGoogleDriveに存在するファイルを検索して、  
 botがファイル結果を通知してくれるアプリケーション
+## 環境
+- ### GoogleApps Script
+- ### GoogleAppsScript Library  
+  - SlackAPI
+- ### Slack
+  - Outgoing Webhook
+
 ## 導入方法
+1. GoogleDriveで適当なファイルを作成し[ツール]→[スクリプトエディタ]を開き  
+`DriveSearcher_.js`の内容をそのまま貼り付け
+
+2. [Slack BotをGASでいい感じで書くためのライブラリを作った](https://qiita.com/soundTricker/items/43267609a870fc9c7453)  
+を参考に設定を行う  
+(↑の記事ではAPI tokenが発行できないので[Slack APIのTokenの取得・場所](https://qiita.com/ykhirao/items/0d6b9f4a0cc626884dbb)を参考にする)
+
+3. スクリプトの`slack_token`をSlack API Tokenのトークンに書き換える
+4. スクリプトの`validToken`をOutgoing Webhookの検証用トークンに置き換える
+5. １度`saveVerifyToken()`と`saveSlackToken()`を実行する
+
 ## 使い方
+1. メッセージに設定したトリガーキーを記述する  
+2. スペースは開けずに、検索したいワードを記述する
+3. ドライブ上にワードが含まれるファイルがあるとbotがそのパスとURLを発言する
